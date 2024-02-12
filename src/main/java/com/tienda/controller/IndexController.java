f2cbfa2f9735cf34c33e2b6d885e559e6dd3d76c
XPC <XPC@DESKTOP-5399KO1>
2024-02-11 21:58:28 -0600

    Finalizacion sem 4

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -5,13 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller//este controlador respondera a todas las peticiones que inicien con index
 public class IndexController {
     
     @RequestMapping("/")
-    public String page(Model model) {
+    //ha esto se le llama metodo o accion
+    public String page(Model model) {//el nombre del metodo se llama page.
         model.addAttribute("attribute", "value");
-        return "index";
+        return "index";//en el return, se indica el nombre de una vista que este en el archivo templates. 
+        //Solo se le agrega el nombre, no el .html
     }
     
 }
